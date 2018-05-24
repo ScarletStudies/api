55400d3a7f64fc3b936a9b240026137f645d0544
Tristan <russon77@gmail.com>
2018-05-24 12:04:02 -0400

    feat: summer 2018 course data migration

---
+++ migrations/versions/9c7b9b1597d0_.py
@@ -0,0 +1,58 @@
+"""courses, term 7, campuses nk nb cm, year 2018
+
+Revision ID: 9c7b9b1597d0
+Revises: 04c0a19ef0c1
+Create Date: 2018-05-24 11:09:56.847834
+
+"""
+from alembic import op
+from sqlalchemy.sql import table, column
+from sqlalchemy import String
+import os
+import json
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '9c7b9b1597d0'
+down_revision = '04c0a19ef0c1'
+branch_labels = None
+depends_on = None
+
+courses_table = table('course',
+                      column('name', String),
+                      column('offering_unit', String),
+                      column('subject', String),
+                      column('course_number', String)
+                      )
+
+paths = (
+    os.path.join(os.path.dirname(__file__), 'courses/7-2018-NK.json'),
+    os.path.join(os.path.dirname(__file__), 'courses/7-2018-NB.json'),
+    os.path.join(os.path.dirname(__file__), 'courses/7-2018-CM.json')
+)
+
+
+def upgrade():
+    for path in paths:
+        with open(path, 'r') as fp:
+            courses = json.load(fp)
+
+            op.bulk_insert(courses_table, courses)
+
+
+def downgrade():
+    for path in paths:
+        with open(path, 'r') as fp:
+            courses = json.load(fp)
+
+            for course in courses:
+                op.execute(
+                    courses_table.update()
+                    .where(
+                        courses_table.name == op.inline_literal(course['name'])
+                        and courses_table.offering_unit == op.inline_literal(course['offering_unit'])
+                        and courses_table.subject == op.inline_literal(course['subject'])
+                        and courses_table.course_number == op.inline_literal(course['course_number'])
+                    )
+                )
